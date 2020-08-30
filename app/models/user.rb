7d4e0cf2b8f45eb84c1449252ee1f50237340fe2
chienleow <chienleow1@gmail.com>
2020-08-30 14:58:51 -0400

    Make changes to User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   has_many :goals, through: :user_goals
   belongs_to :team
 
-  validates [:first_name, :last_name, :username], :uniqueness => true, :presence => true
+  validates [:first_name, :last_name, :username], :presence => true
+  validates :username, :uniqueness => true
 
   has_secure_password
 end
